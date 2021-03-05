317aa250ff8bdfc759e568639fdf45a9bdc56d40
Muthazagan <muthazagan123@gmail.com>
2021-03-05 15:48:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-amzvnj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-amzvnj)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  powers = ["Really Smart", "Super Flexible", "Weather Changer"];
+  //hero = { name: "Dr.", alterEgo: "Dr. What", power: this.powers[0] };
+  test = { name: "Dr.", alterEgo: "Dr. What", power: this.powers[0] };
+}
