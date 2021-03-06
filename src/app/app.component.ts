5da7fe2d94b6f59bce617fa06cd870b1c554e98e
Muthazagan <muthazagan123@gmail.com>
2021-03-06 10:52:37 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, VERSION } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { FormBuilder, FormGroup } from "@angular/forms";
-import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "my-app",
@@ -20,19 +18,7 @@ export class AppComponent {
     mobile: "",
     file: [""]
   };
-  SERVER_URL = "http://localhost:3000/upload";
-  testForm: FormGroup;
   file: any;
-  constructor(
-    private formBuilder: FormBuilder,
-    private httpClient: HttpClient
-  ) {}
-  ngOnInit() {
-    this.testForm = this.formBuilder.group({
-      profile: [""]
-    });
-  }
-
   onSubmit(testForm: NgForm) {
     console.log(testForm.value.name); // { first: '', last: '' }
     console.log(testForm.value); // false
@@ -45,12 +31,5 @@ export class AppComponent {
       return false;
     }
     return true;
-  }
-
-  onFileSelect(event) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.testForm.get("profile").setValue(file);
-    }
   }
 }
